37059102029cba95aaf9052a11e75119a834a012
Alexei <a.kovalkovsky@gmail.com>
2021-05-19 02:35:44 +0300

    parse

--- addProduct.js
+++ addProduct.js
@@ -7,8 +7,8 @@ btn.forEach( (item) =>{
         
       obj.name =  $(this).closest('.one-product').find('.product-title').text();
       obj.price = $(this).closest('.one-product').find('.product-price').text();
-      
-      localStorage.setItem('product',  JSON.stringify(obj));
+      arr.push(obj);
+      localStorage.setItem('product',  JSON.stringify(arr));
       
       
     })
